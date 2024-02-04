80d306cd74b4387ee184c145221c39bf372f74ae
Itbrandon <brandoniticka@gmail.com>
2024-02-03 16:21:25 -0800

    async-await

--- main.js
+++ main.js
@@ -7,51 +7,102 @@ let stocks = {
 
 let storeIsOpen = true;
 
-order = (time,work) => {
+function time(ms) {
 
   return new Promise((resolve, reject) => {
-
     if(storeIsOpen)
-  {
-    setTimeout(() => {
-      resolve(work());
-    },time)
-  }
-
-  else {
-    reject(console.log("We are Closed"));
-  }
-
-
+    {
+      setTimeout(resolve,ms);
+    }
+    else
+    {
+      reject(console.log("We are Closed"))
+    }
   })
 }
 
-order(2000, () => console.log(`You Selected ${stocks.Fruits[0]}`))
+async function kitchen() {
+ try {
+  await time(2000);
+  console.log(`You Selected ${stocks.Fruits[0]}`);
 
-.then(() => {
-  return order(0, () => console.log(`Production has Started`));
-})
-.then(() => {
-  return order(2000, () => console.log(`Cutting the Fruit`));
-})
-.then(() => {
-  return order(1000, () => console.log(`Added the ${stocks.liquid[0]} and ${stocks.liquid[1]}`));
-})
-.then(() => {
-  return order(1000, () => console.log(`Started the machine`));
-})
-.then(() => {
-  return order(2000, () => console.log(`Put Ice Cream in ${stocks.holder[0]}`));
-})
-.then(() => {
-  return order(3000, () => console.log(`Added ${stocks.toppings[0]}s to it`));
-})
-.then(() => {
-  return order(2000, () => console.log(`Here's your Ice Cream`));
-})
-.catch(() => {
+  await time(0);
+  console.log(`Production has Started`);
+
+  await time(2000);
+  console.log(`Cutting the Fruit`);
+
+  await time(1000);
+  console.log(`Added the ${stocks.liquid[0]} and ${stocks.liquid[1]}`);
+
+  await time(1000);
+  console.log(`Started the machine`);
+
+  await time(2000);
+  console.log(`Put Ice Cream in ${stocks.holder[0]}`);
+
+  await time(3000);
+  console.log(`Added ${stocks.toppings[0]}s to it`);
+
+  await time(2000);
+  console.log(`Here's your Ice Cream`)
+ }
+ catch(error) {
   console.log(`No Customers in Store`);
-})
-.finally(() => {
+ }
+
+ finally {
   console.log(`Have a Great Day`);
-})
+ }
+}
+
+kitchen();
+
+// order = (time,work) => {
+
+//   return new Promise((resolve, reject) => {
+
+//     if(storeIsOpen)
+//   {
+//     setTimeout(() => {
+//       resolve(work());
+//     },time)
+//   }
+
+//   else {
+//     reject(console.log("We are Closed"));
+//   }
+
+
+//   })
+// }
+
+// order(2000, () => console.log(`You Selected ${stocks.Fruits[0]}`))
+
+// .then(() => {
+//   return order(0, () => console.log(`Production has Started`));
+// })
+// .then(() => {
+//   return order(2000, () => console.log(`Cutting the Fruit`));
+// })
+// .then(() => {
+//   return order(1000, () => console.log(`Added the ${stocks.liquid[0]} and ${stocks.liquid[1]}`));
+// })
+// .then(() => {
+//   return order(1000, () => console.log(`Started the machine`));
+// })
+// .then(() => {
+//   return order(2000, () => console.log(`Put Ice Cream in ${stocks.holder[0]}`));
+// })
+// .then(() => {
+//   return order(3000, () => console.log(`Added ${stocks.toppings[0]}s to it`));
+// })
+// .then(() => {
+//   return order(2000, () => console.log(`Here's your Ice Cream`));
+// })
+// .catch(() => {
+//   console.log(`No Customers in Store`);
+// })
+// .finally(() => {
+//   console.log(`Have a Great Day`);
+// })
